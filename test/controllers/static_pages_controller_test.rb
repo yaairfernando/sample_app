860c797245cdd82ab06c140d2e56c0d90aace9bf
Yair Facio <yair.facio11@gmail.com>
2020-01-28 15:30:08 -0600

    Fixing rubocop

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,10 +12,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   #   assert_response FILL_IN
   # end
 
-  test "should get home" do
+  test 'should get home' do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select 'title', 'Ruby on Rails Tutorial Sample App'
   end
 
   test 'should get help' do

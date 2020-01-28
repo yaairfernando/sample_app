e84ee2978b09d6e203bdfcd98c98b090407acbc6
Yair Facio <yair.facio11@gmail.com>
2020-01-28 14:51:48 -0600

    Refactoring css to scss and adding some tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,24 +12,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   #   assert_response FILL_IN
   # end
 
-  test 'should get home' do
-    get static_pages_home_url
+  test "should get home" do
+    get root_path
     assert_response :success
-    assert_select 'title', 'Ruby on Rails Tutorial Sample App'
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test 'should get help' do
-    get static_pages_help_url
+    get help_path
     assert_response :success
   end
 
   test 'should get about' do
-    get static_pages_about_url
+    get about_path
     assert_response :success
   end
 
   test 'should get contact' do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select 'title', "Contact | #{@title}"
   end

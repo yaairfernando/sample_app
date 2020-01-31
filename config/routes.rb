84af4b3b9d893731f5f95c46e4a6fc2e2d5c14f2
Yair Facio <yair.facio11@gmail.com>
2020-01-31 14:48:02 -0600

    Finished user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users
+  resources :users, except: %i[new]
   resources :posts
   root 'static_pages#home'
   get 'signup', to: 'users#new'

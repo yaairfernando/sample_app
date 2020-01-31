b1de7c05582f2b8ad0726a55bfb0d4006fa0ca55
Yair Facio <yair.facio11@gmail.com>
2020-01-31 15:11:57 -0600

    Upgrading migrations

--- db/migrate/20200131211018_create_posts_table.rb
+++ db/migrate/20200131211018_create_posts_table.rb
@@ -1,10 +1,8 @@
-class CreatePosts < ActiveRecord::Migration[5.2]
+class CreatePostsTable < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string :body
       t.references :user, foreign_key: true
-
-      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_174510) do
+ActiveRecord::Schema.define(version: 2020_01_31_211018) do
 
   create_table "posts", force: :cascade do |t|
     t.string "body"
     t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -24,8 +22,6 @@ ActiveRecord::Schema.define(version: 2020_01_31_174510) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email"
   end
 

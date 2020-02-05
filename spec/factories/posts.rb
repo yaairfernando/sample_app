a8459efb7326ac2393f8f3acd781c3a00a429b8d
Yair Facio <yair.facio11@gmail.com>
2020-02-05 14:18:16 -0600

    Finished post model specs

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,4 +3,9 @@ FactoryBot.define do
     body { "This is my first post" }
     user_id { 1 }
   end
+
+  factory :random_post, class: Post do
+    body { Faker::Lorem }
+    user_id { Faker::Number.unique}
+  end
 end

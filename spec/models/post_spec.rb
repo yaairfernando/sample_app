eb889f3f2cbe5dc8f0acb6444540dc1a39a17810
Yair Facio <yair.facio11@gmail.com>
2020-02-05 15:42:42 -0600

    Adding more validations to the post model spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,10 +7,17 @@ RSpec.describe Post, type: :model do
     let(:post) { build(:post, user_id: user.id)}
 
     it 'ensures a body is present' do
-      post.body = nil
+      post.body = "a" * 2
       expect(post.save).to eq(false)
       expect(post).not_to be_valid
     end
+
+    it 'ensures a body is not too long' do
+      post.body = "a" * 141
+      expect(post.save).to eq(false)
+      expect(post).not_to be_valid
+    end
+    
 
     it 'ensures a user_id is present' do
       post.user_id = nil

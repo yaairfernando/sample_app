01d1f2ffc97957189e7d03f7730b0d53369978f8
Yair Facio <yair.facio11@gmail.com>
2020-02-06 15:25:29 -0600

    Finished log_in helper method for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
     # render plain: params[:session].inspect
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      render html: 'Yes'
+      log_in user
+      redirect_to user
+      # render html: 'Yes'
     else
       flash.now[:danger] = "Password or email are invalid!!.. Try again please"
       render 'new'

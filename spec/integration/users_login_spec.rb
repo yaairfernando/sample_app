c2646f258f2dcaf8246b7d60a4043bc927f323c9
Yair Facio <yair.facio11@gmail.com>
2020-02-07 15:26:24 -0600

    Adding a test to check when the user logs out of the application

--- spec/integration/users_login_spec.rb
+++ spec/integration/users_login_spec.rb
@@ -34,5 +34,28 @@ describe 'User Login', type: :request do
       assert_select 'a[href=?]', logout_path, count: 0
       assert_select 'a[href=?]', user_path(user), count: 0
     end
+
+    it 'login with valid information followed by logout' do
+      user = create(:user_login, email: 'yair.facio11@gmail.com', password: 'password')
+      get login_path
+      post login_path, params: { session: { email: user.email, password: 'password' } }
+      assert is_logged_in?
+      assert_redirected_to user
+      follow_redirect!
+      assert_template 'users/show'
+      assert_select "a[href=?]", login_path, count: 0
+      assert_select "a[href=?]", logout_path
+      assert_select "a[href=?]", user_path(user)
+      delete logout_path
+      expect(is_logged_in?).to eq(false)
+      assert_redirected_to root_path
+
+      delete logout_path
+      follow_redirect!
+      assert_select "a[href=?]", login_path
+      assert_select "a[href=?]", logout_path, count: 0
+      assert_select "a[href=?]", user_path(user), count: 0
+    end
+    
   end
 end

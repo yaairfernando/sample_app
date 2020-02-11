c99d2332f8499eb82b1edb4d0960242fe2e28db9
Yair Facio <yair.facio11@gmail.com>
2020-02-11 16:44:46 -0600

    Adding delivery_method to smpt

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    ActionMailer::Base.delivery_method = :smtp
     # Settings in config/environments/* take precedence
     # over those specified here.
     # Application configuration can go into files in config/initializers

c4edaea4491825ed43f6b093c912215f61b7e4c5
Yair Facio <yair.facio11@gmail.com>
2020-02-11 14:22:07 -0600

    Genereated controller password_resets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'static_pages#home'
   get 'help', to: "static_pages#help"
   get 'about', to: "static_pages#about"
@@ -15,6 +17,7 @@ Rails.application.routes.draw do
   resources :users, except: %i[new]
   resources :posts
   resources :account_activations, only: %i[edit]
+  resources :password_resets, only: %i[new create edit update]
 
   default_url_options :host => "localhost:3000"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

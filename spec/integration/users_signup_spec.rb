03bf64b82a59968c922b68552fe5589b3985afc6
Yair Facio <yair.facio11@gmail.com>
2020-02-11 13:06:04 -0600

    Adding test for signup with account activation

--- spec/integration/users_signup_spec.rb
+++ spec/integration/users_signup_spec.rb
@@ -14,6 +14,11 @@ describe 'User SinUp', type: :request do
       password: 'pas',
       password_confirmation: 'bar' }
   end
+
+  before(:all) do
+    ActionMailer::Base.deliveries.clear
+  end
+
   describe 'POST #SignUp' do
 
     it 'invalid signup information' do
@@ -23,6 +28,24 @@ describe 'User SinUp', type: :request do
       assert_select 'div.alert-danger'
       assert_select 'li', count: 10
     end
+
+    it 'valid signup information with account activation' do
+      get signup_path
+      post users_path, params: { user: valid_attributes }
+      expect(ActionMailer::Base.deliveries.size).to eq(1)
+      user = assigns(:user)
+      expect(user.activated?).to eq(false)
+      log_in_as(user)
+      expect(is_logged_in?).to eq(false)
+      get edit_account_activation_path(user.activation_token, email: 'wrong')
+      expect(is_logged_in?).to eq(false)
+      get edit_account_activation_path(user.activation_token, email: user.email)
+      assert user.reload.activated?
+      follow_redirect!
+      assert_template 'users/show'
+      expect(is_logged_in?).to eq(true)
+    end
+    
 
     it 'valid signup information' do
       get signup_path

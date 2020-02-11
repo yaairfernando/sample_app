b137b2d3fcefe6216fdc4282f87f5154b75a0b4b
Yair Facio <yair.facio11@gmail.com>
2020-02-11 11:49:47 -0600

    Redirecting the user to the root_path if they have not activated their account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,18 @@ class SessionsController < ApplicationController
   def create
     # render plain: params[:session].inspect
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_back_or user
-      # render html: 'Yes'
+    if user && user.authenticate(params[:session][:password])
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+        redirect_back_or user
+        # render html: 'Yes'
+      else
+        message = "Account not activated."
+        message += "Check you email for the activation link."
+        flash[:warning] = message
+        redirect_to root_path
+      end
     else
       flash.now[:danger] = 'Password or email are invalid!!.. Try again please'
       render 'new'

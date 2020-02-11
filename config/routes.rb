5ad13fbe8ad2538676cc49f19a98df3052f40d5b
Yair Facio <yair.facio11@gmail.com>
2020-02-11 11:04:29 -0600

    Testing the account_activation method with rspec

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
   resources :users, except: %i[new]
   resources :posts
   resources :account_activations, only: %i[edit]
+
+  default_url_options :host => "localhost:3000"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,16 +2,18 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "account_activation" do
-    let(:mail) { UserMailer.account_activation }
-
+    let(:user) { create(:user_activated)}
     it "renders the headers" do
-      expect(mail.subject).to eq("Account activation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
-    end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      user = create(:user)
+      user.activation_token = User.new_token
+      mailer = UserMailer.account_activation(user)
+      expect(mailer.subject).to eq("Account activation")
+      # expect(mailer.to).to eq(user.email)
+      expect(mailer.from).to eq(["noreply@example.com"])
+      expect(mailer.body.encoded).to match(user.name)
+      expect(mailer.body.encoded).to match(user.activation_token)
+      # expect(CGI.escape(user.email)).to eq(mailer.body.encoded)
+      expect(mailer.body.encoded).to match("Hi")
     end
   end
 
@@ -21,7 +23,7 @@ RSpec.describe UserMailer, type: :mailer do
     it "renders the headers" do
       expect(mail.subject).to eq("Password reset")
       expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.from).to eq(["noreply@example.com"])
     end
 
     it "renders the body" do

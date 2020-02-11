fcef69dda8641fb2d0c386765900b53d85a117ee
Yair Facio <yair.facio11@gmail.com>
2020-02-11 14:36:53 -0600

    Created view for reset password and create method inside password_resets_controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,19 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "An email has been sent to the specified email address with intructions to reset your password"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "The email address provided was not found"
+      render :new
+    end
+  end
+
   def edit
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_153609) do
+ActiveRecord::Schema.define(version: 2020_02_11_202659) do
 
   create_table "posts", force: :cascade do |t|
     t.string "body"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_02_11_153609) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email"
   end
 
